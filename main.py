6eb4a8a1b1df4f08ae31170982b98c773ca3383d
Mehdi Imeni <imeni1982@gmail.com>
2025-05-03 12:26:37 +0330

    learn fastapi start

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 
 
@@ -5,8 +6,31 @@ app = FastAPI()
 
 @app.get("/")
 def index():
-    return {"data": {'name': 'Index page'}}
+    return {"data": 'Index page'}
 
 @app.get("/about")
 def about():
-    return {"data": {'name': 'About page'}}
+    return {"data":'About page'}
+
+
+@app.get("/blog")
+def all_blogs(limit:int=10,published:bool=True, sort: Optional[str]=None):
+    
+    if published:
+        return {"data": f'All {limit} published blogs from the database'}
+    else:
+        return {"data": f'Blog page unpublished limit : {limit} '}
+
+
+@app.get("/blog/unpublished")
+def unpublished():
+    return {"data": "all unpublished blogs"}
+
+@app.get("/blog/{id}")
+def show(id:int):
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+def comments(id:int,limit:int=10):
+    return {"data": f'{id} , {limit}'}
