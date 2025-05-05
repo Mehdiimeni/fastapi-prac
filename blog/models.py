264a089f7c72e130391aac121860f790c46ed617
Mehdi Imeni <imeni1982@gmail.com>
2025-05-05 09:32:43 +0330

    end part 1

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Integer, String
+from sqlalchemy import Integer, String, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column
 from .database import Base
+from sqlalchemy.orm import relationship
 
 class Blog(Base):
     __tablename__ = "blogs"
@@ -8,3 +9,17 @@ class Blog(Base):
     id: Mapped[int] = mapped_column(primary_key=True, index=True)
     title: Mapped[str] = mapped_column(String)
     body: Mapped[str] = mapped_column(String)
+    user_id: Mapped[int] = mapped_column(Integer, ForeignKey("users.id"))
+    creator = relationship("User", back_populates="blogs")
+    
+    
+class User(Base):
+    __tablename__ = "users"
+    
+    id: Mapped[int] = mapped_column(primary_key=True, index=True)
+    name: Mapped[str] = mapped_column(String)
+    email: Mapped[str] = mapped_column(String)
+    password: Mapped[str] = mapped_column(String)
+    
+    blogs = relationship("Blog", back_populates="creator")
+    
