e2c8687f91cbe1e820bd47e424cb646404499cbd
Mehdi Imeni <imeni1982@gmail.com>
2025-05-05 11:17:36 +0330

    end part 1

--- blog/routers/blog.py
+++ blog/routers/blog.py
@@ -2,62 +2,37 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from .. import schemas, models, database
 from typing import List
 from sqlalchemy.orm import Session
+from ..repository import blog_repository as repository
 
-router = APIRouter()
+router = APIRouter(prefix="/blog", tags=["Blogs"])
 
 get_db = database.get_db
 
 
-@router.get("/blog",
+@router.get("/",
             response_model=List[schemas.ShowBlog],
+            status_code=status.HTTP_200_OK)
+def get_all(db: Session = Depends(get_db)):
+    return repository.get_all(db)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    return repository.create(request, db)
+
+
+@router.get("/{id}",
             status_code=status.HTTP_200_OK,
-            tags=["blogs"])
-def get_all_blogs(db: Session = Depends(get_db)):
-    blogs = db.query(models.Blog).all()
-    return blogs
+            response_model=schemas.ShowBlog)
+def show(id: int, db: Session = Depends(get_db)):
+    return repository.show(id, db)
 
 
-@router.post("/blog", status_code=status.HTTP_201_CREATED, tags=["blogs"])
-def create_blog(blog: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=blog.title, body=blog.body, user_id=1)
-    db.add(new_blog)
-    db.commit()
-    db.refresh(new_blog)
-    return new_blog
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.Blog, db: Session = Depends(get_db)):
+    return repository.update(id, request, db)
 
 
-@router.delete("/blog/{id}",
-               status_code=status.HTTP_204_NO_CONTENT,
-               tags=["blogs"])
-def delete_blog(id: int, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Blog with id {id} not found")
-    db.delete(blog)
-    db.commit()
-    return  # 204 No Content should not return a body
-
-
-@router.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED, tags=["blogs"])
-def update_blog(id: int, blog: schemas.Blog, db: Session = Depends(get_db)):
-    blog_to_update = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog_to_update:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Blog with id {id} not found")
-    blog_to_update.title = blog.title
-    blog_to_update.body = blog.body
-    db.commit()
-    return "blog_to_update"
-
-
-@router.get("/blog/{id}",
-            status_code=status.HTTP_200_OK,
-            response_model=schemas.ShowBlog,
-            tags=["blogs"])
-def get_blog(id: int, db: Session = Depends(get_db)):
-    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
-    if not blog:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"Blog with id {id} not found")
-    return blog
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    return repository.destroy(id, db)
--- blog/routers/user.py
+++ blog/routers/user.py
@@ -2,66 +2,40 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from .. import schemas, models, database, hashing
 from typing import List
 from sqlalchemy.orm import Session
+from ..repository import user_repository as repository
 
 get_db = database.get_db
 Hash = hashing.Hash
 
-router = APIRouter()
+router = APIRouter(prefix="/user", tags=["Users"])
 
 
-@router.post("/user", status_code=status.HTTP_201_CREATED, tags=["users"])
-def create_user(user: schemas.User, db: Session = Depends(get_db)):
-    hashed_password = Hash.bcrypt(user.password)
-    new_user = models.User(name=user.name,
-                           email=user.email,
-                           password=hashed_password)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+@router.get(
+    "/",
+    status_code=status.HTTP_200_OK,
+    response_model=List[schemas.ShowUser],
+)
+def get_all(db: Session = Depends(get_db)):
+    return repository.get_all(db)
 
 
-@router.get("/user",
+@router.post("/", status_code=status.HTTP_201_CREATED)
+def create(request: schemas.User, db: Session = Depends(get_db)):
+    return repository.create(request, db)
+
+
+@router.get("/{id}",
             status_code=status.HTTP_200_OK,
-            response_model=List[schemas.ShowUser],
-            tags=["users"])
-def get_all_users(db: Session = Depends(get_db)):
-    users = db.query(models.User).all()
-    return users
+            response_model=schemas.ShowUser)
+def show(id: int, db: Session = Depends(get_db)):
+    return repository.show(id, db)
 
 
-@router.get("/user/{id}",
-            status_code=status.HTTP_200_OK,
-            response_model=schemas.ShowUser,
-            tags=["users"])
-def get_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User with id {id} not found")
-    return user
+@router.put("/{id}", status_code=status.HTTP_202_ACCEPTED)
+def update(id: int, request: schemas.User, db: Session = Depends(get_db)):
+    return repository.update(id, request, db)
 
 
-@router.delete("/user/{id}",
-               status_code=status.HTTP_204_NO_CONTENT,
-               tags=["users"])
-def delete_user(id: int, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.id == id).first()
-    if not user:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User with id {id} not found")
-    db.delete(user)
-    db.commit()
-    return "user deleted"
-
-
-@router.put("/user/{id}", status_code=status.HTTP_202_ACCEPTED, tags=["users"])
-def update_user(id: int, user: schemas.User, db: Session = Depends(get_db)):
-    user_to_update = db.query(models.User).filter(models.User.id == id).first()
-    if not user_to_update:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"User with id {id} not found")
-    user_to_update.email = user.email
-    user_to_update.name = user.name
-    db.commit()
-    return "user successful updated"
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def destroy(id: int, db: Session = Depends(get_db)):
+    return repository.destroy(id, db)
